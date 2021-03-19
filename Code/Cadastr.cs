e9ca81b4235472388768cc55833c3cbcb4245419
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2021-03-19 09:58:24 +0300

    Create Cadastr.cs

---
+++ Code/Cadastr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Civil.ApplicationServices;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using OSGeo.MapGuide;
+using Autodesk.AutoCAD.Windows;
+using Autodesk.DesignScript.Runtime;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+using System.Xml.Linq;
+using System.Reflection;
+
+namespace MapConnection
+{
+	public class Cadastr
+	{
+		private Cadastr() { }
+		public static void GML_LinePolygonsIntersection(string GML_PathToLine, string GML_PathToPolygons)
+		{
+     
+    }
+
+	}
+}
